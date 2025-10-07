0b879a466f71e3ed45b0e085b74f36b75c4219e9
Brian Hart <bbhart@bbhart.com>
2025-10-07 10:26:43 -0400

    updated servelocal to work with this new fun ruby cert crl problem

--- servelocal.sh
+++ servelocal.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-bundle exec jekyll serve --baseurl=""
+# 2025-Oct-07 This is the new command to avoid ruby CRL fuckery; I spent like an hour on this :| 
+bundle exec ruby -ropenssl -e 'OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE; load Gem.bin_path("jekyll","jekyll")' serve
+
+# The previous command
+# bundle exec jekyll serve --baseurl=""
